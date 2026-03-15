d171540a1767f8c307dcbbdba08b3da52bacbae7
Good'n Paul <73657550+Goodn6138@users.noreply.github.com>
2026-03-15 18:56:08 +0300

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,1 +1,32 @@
+from flask import Flask, request, send_file
+import subprocess
+import os
 
+app = Flask(__name__)
+
+@app.route("/compile", methods=["POST"])
+def compile():
+
+    code = request.json["code"]
+
+    os.makedirs("sketch", exist_ok=True)
+
+    with open("sketch/sketch.ino","w") as f:
+        f.write(code)
+
+    subprocess.run([
+        "arduino-cli",
+        "compile",
+        "--fqbn",
+        "esp32:esp32:esp32",
+        "sketch"
+    ])
+
+    firmware = "sketch/build/esp32.esp32.esp32/sketch.ino.bin"
+
+    return send_file(
+        firmware,
+        mimetype="application/octet-stream"
+    )
+
+app.run(port=5000)
